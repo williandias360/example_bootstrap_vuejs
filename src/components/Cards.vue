6a6e8b1ac96eef6ac3c68505b03731443dc2cac7
Willian Dias Brito <willian.dias360@gmail.com>
2019-10-06 17:35:15 -0300

    Primeiro commit

---
+++ src/components/Cards.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md">
+        <div
+          class="card text-white bg-primary mb-3"
+          style="max-width: 18rem;"
+          v-for="card in cards"
+          v-bind:key="card"
+        >
+          <div class="card-header">Header</div>
+          <div class="card-body">
+            <h5 class="card-title">Primary card title</h5>
+            <p class="card-text">{{ card }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cards",
+  data: function() {
+    return {
+      cards: ["Descrição 1", "Descrição 2", "Descrição 3", "Descrição 4"]
+    };
+  },
+  methods: {}
+};
+</script>
