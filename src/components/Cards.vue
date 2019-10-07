f747c835a0e751c80ee39d25bcf1676d0e06c7ca
Willian Dias <willian.dias360@gmail.com>
2019-10-07 13:46:11 -0300

    Ajuste dos cards

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-md">
+    <div class="row" >
+      <div class="col-12 col-sm-6 col-md-4 col-lg-3 justify-content-center"  v-for="card in cards"
+          v-bind:key="card">
         <div
-          class="card text-white bg-primary mb-3"
-          style="max-width: 18rem;"
-          v-for="card in cards"
-          v-bind:key="card"
-        >
+          class="card text-white bg-primary mb-3" style="max-width: 18rem;">
           <div class="card-header">Header</div>
           <div class="card-body">
             <h5 class="card-title">Primary card title</h5>
@@ -24,7 +21,8 @@ export default {
   name: "Cards",
   data: function() {
     return {
-      cards: ["Descrição 1", "Descrição 2", "Descrição 3", "Descrição 4"]
+      cards: ["Descrição 1", "Descrição 2", "Descrição 3", "Descrição 4",
+      "Descrição 5", "Descrição 6", "Descrição 7", "Descrição 8", "Descrição 9", "Descrição 10"]
     };
   },
   methods: {}
