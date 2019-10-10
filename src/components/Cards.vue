b2f4e1df4e15901e81cfdbf9afad558382799ac3
Willian Dias <willian.dias360@gmail.com>
2019-10-10 13:51:27 -0300

    Adicionado classe de status aos cards

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-12 col-md-6 col-lg-6 col-xl-4" v-for="card in cards" v-bind:key="card">
-        <div class="card text-white bg-primary mb-3" style="max-width: 30rem;" v-on:click="clickOnCard(card)">
+        <div class="card text-white mb-3" v-bind:class="{'bg-danger' :aguardando, 'bg-success':encaminhado, 'bg-secondary':finalizado}" style="max-width: 30rem;" v-on:click="clickOnCard(card)">
           <div class="card-header">Header</div>
           <div class="card-body">
             <h5 class="card-title">Primary card title</h5>
@@ -19,6 +19,9 @@ export default {
   name: "Cards",
   data: function() {
     return {
+      aguardando:true,
+      encaminhado:true,
+      finalizado:true,
       cards: [
         "Descrição 1",
         "Descrição 2",
@@ -31,7 +34,17 @@ export default {
         "Descrição 9",
         "Descrição 10",
         "Descrição 11",
-        "Descrição 12"
+        "Descrição 12",
+        "Descrição 13",
+        "Descrição 14",
+        "Descrição 15",
+        "Descrição 16",
+        "Descrição 17",
+        "Descrição 18",
+        "Descrição 19",
+        "Descrição 20",
+        "Descrição 21",
+        "Descrição 22",
       ]
     };
   },
