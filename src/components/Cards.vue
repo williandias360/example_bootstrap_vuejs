af079e76394f34b1e45d7d027a096e870937c387
Willian Dias <willian.dias360@gmail.com>
2019-10-10 13:37:23 -0300

    Corrigido alinhamento dos cards

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="container">
-    <div class="row" >
-      <div class="col-12 col-sm-6 col-md-4 col-lg-3 justify-content-center"  v-for="card in cards"
-          v-bind:key="card">
-        <div
-          class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+    <div class="row">
+      <div class="col-12 col-md-6 col-lg-6 col-xl-4" v-for="card in cards" v-bind:key="card">
+        <div class="card text-white bg-primary mb-3" style="max-width: 30rem;" v-on:click="clickOnCard(card)">
           <div class="card-header">Header</div>
           <div class="card-body">
             <h5 class="card-title">Primary card title</h5>
@@ -21,10 +19,26 @@ export default {
   name: "Cards",
   data: function() {
     return {
-      cards: ["Descrição 1", "Descrição 2", "Descrição 3", "Descrição 4",
-      "Descrição 5", "Descrição 6", "Descrição 7", "Descrição 8", "Descrição 9", "Descrição 10"]
+      cards: [
+        "Descrição 1",
+        "Descrição 2",
+        "Descrição 3",
+        "Descrição 4",
+        "Descrição 5",
+        "Descrição 6",
+        "Descrição 7",
+        "Descrição 8",
+        "Descrição 9",
+        "Descrição 10",
+        "Descrição 11",
+        "Descrição 12"
+      ]
     };
   },
-  methods: {}
+  methods: {
+    clickOnCard(card){
+      alert(`Clicou no card ${card}`);
+    }
+  }
 };
 </script>
