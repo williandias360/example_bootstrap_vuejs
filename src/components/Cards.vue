2aa436423fba8cdf44c93978521b9400e71d8799
Willian Dias <willian.dias360@gmail.com>
2019-10-11 13:46:19 -0300

    Criado objetos para listagem dos cards

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-12 col-md-6 col-lg-6 col-xl-4" v-for="card in cards" v-bind:key="card">
-        <div class="card text-white mb-3" v-bind:class="{'bg-danger' :aguardando, 'bg-success':encaminhado, 'bg-secondary':finalizado}" style="max-width: 30rem;" v-on:click="clickOnCard(card)">
-          <div class="card-header">Header</div>
+      <div class="col-12 col-md-6 col-lg-6 col-xl-4" v-for="card in cards" v-bind:key="card.CodigoAtendimento">
+        <div
+          class="card text-white mb-3"
+          v-bind:class="[card.Situacao == 'Aguardando' ? 'bg-danger' : card.Situacao == 'Encaminhado' ? 'bg-success' : 'bg-secondary']"
+          style="max-width: 30rem;"
+          v-on:click="clickOnCard(card)"
+        >
+          <div class="card-header">{{card.Situacao}}</div>
           <div class="card-body">
-            <h5 class="card-title">Primary card title</h5>
-            <p class="card-text">{{ card }}</p>
+            <h5 class="card-title">{{card.Titulo}}</h5>
+            <p class="card-text">{{ card.Descricao }}</p>
+            <p class="card-text">{{ card.DataSolicitacao }}</p>
           </div>
         </div>
       </div>
@@ -15,41 +21,59 @@
 </template>
 
 <script>
+
 export default {
   name: "Cards",
   data: function() {
     return {
-      aguardando:true,
-      encaminhado:true,
-      finalizado:true,
       cards: [
-        "Descrição 1",
-        "Descrição 2",
-        "Descrição 3",
-        "Descrição 4",
-        "Descrição 5",
-        "Descrição 6",
-        "Descrição 7",
-        "Descrição 8",
-        "Descrição 9",
-        "Descrição 10",
-        "Descrição 11",
-        "Descrição 12",
-        "Descrição 13",
-        "Descrição 14",
-        "Descrição 15",
-        "Descrição 16",
-        "Descrição 17",
-        "Descrição 18",
-        "Descrição 19",
-        "Descrição 20",
-        "Descrição 21",
-        "Descrição 22",
+        {
+          CodigoAtendimento: 1,
+          Titulo: "Titulo do atendimento",
+          Descricao: "Descrição do atendimento",
+          Situacao: "Aguardando",
+          DataSolicitacao: Date()
+        },
+        {
+          CodigoAtendimento: 2,
+          Titulo: "Titulo do atendimento2",
+          Descricao: "Descrição do atendimento2",
+          Situacao: "Aguardando",
+          DataSolicitacao: Date()
+        },
+        {
+          CodigoAtendimento: 3,
+          Titulo: "Titulo do atendimento3",
+          Descricao: "Descrição do atendimento3",
+          Situacao: "Encaminhado",
+          DataSolicitacao: Date()
+        },
+        {
+          CodigoAtendimento: 4,
+          Titulo: "Titulo do atendimento4",
+          Descricao: "Descrição do atendimento4",
+          Situacao: "Encaminhado",
+          DataSolicitacao: Date()
+        },
+        {
+          CodigoAtendimento: 5,
+          Titulo: "Titulo do atendimento5",
+          Descricao: "Descrição do atendimento5",
+          Situacao: "Finalizado",
+          DataSolicitacao: Date()
+        },
+        {
+          CodigoAtendimento: 6,
+          Titulo: "Titulo do atendimento6",
+          Descricao: "Descrição do atendimento6",
+          Situacao: "Finalizado",
+          DataSolicitacao: Date()
+        }
       ]
     };
   },
   methods: {
-    clickOnCard(card){
+    clickOnCard(card) {
       alert(`Clicou no card ${card}`);
     }
   }
